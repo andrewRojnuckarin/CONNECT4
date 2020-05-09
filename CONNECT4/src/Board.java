9a7200d918198adf79ff8ac9a4e2c5d1b52bb82a
andrewRojnuckarin <49501067+andrewRojnuckarin@users.noreply.github.com>
2020-05-09 17:14:17 -0400

    lmfao

--- CONNECT4/src/Board.java
+++ CONNECT4/src/Board.java
@@ -1,15 +1,21 @@
 
 public class Board {
 
-	private static final boolean RED = true;
-	private static final boolean BLACK = false;
+	private static final int RED = 1;
+	private static final int EMPTY = 0;
+	private static final int BLACK = -1;
 	
-	private boolean[][] array;
-	private boolean turn;
+	private int[][] array;
+	private int turn;
 	private int turnNum;
 	
-	public Board() {
-		array = new boolean[7][6];
+	public Board(int h, int w) {
+		array = new int[h][w];
+		for (int i = 0; i < array.length; i++) {
+			for (int j = 0; j < array[0].length; j++) {
+				array[i][j] = EMPTY;
+			}
+		}
 		turn = RED;
 		turnNum = 0;
 	}
@@ -23,11 +29,39 @@ public class Board {
 	}
 	
 	public void play(int col) {
-		
+		for (int i = array.length - 1; i > -1; i--) {
+			if (array[i][col] == EMPTY) {
+				array[i][col] = turn;
+				i = -1;
+			}
+		}
+		turn = turn * -1;
+		turnNum++;
 	}
 	
 	public void printBoard() {
-		
+		for (int i = 0; i < array[0].length; i++) {
+			System.out.print(" " + i + " ");
+		}
+		System.out.println();
+		System.out.println();
+		for (int i = 0; i < array.length; i++) {
+			for (int j = 0; j < array[0].length; j++) {
+				if (array[i][j] == EMPTY) {
+					System.out.print(" o ");
+				} else if (array[i][j] == RED) {
+					System.out.print(" R ");
+				} else if (array[i][j] == BLACK) {
+					System.out.print(" B ");
+				}
+			}
+			System.out.println();
+			System.out.println();
+		}
+	}
+	
+	public int getTurnNum() {
+		return turnNum;
 	}
 	
 	public int getBestMove() {
--- CONNECT4/src/Driver.java
+++ CONNECT4/src/Driver.java
@@ -1,6 +1,11 @@
 
 public class Driver {
 	public static void main(String args[]) {
-		
+		Board C4 = new Board(6, 7);
+		C4.printBoard();
+		C4.play(0);
+		C4.printBoard();
+		C4.play(0);
+		C4.printBoard();
 	}
 }
